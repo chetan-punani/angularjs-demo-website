5f69f5a9194e1442e1f834a476d5caa956783930
chetan <16it099@charusat.edu.in>
2022-11-02 12:52:32 +0530

    updated changes

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -9,4 +9,7 @@ export class FooterComponent {
   address: string = '15-16, Times Corporation,Theltej-Silaj Road, Ahemdabad';
   number: number = +1234567890;
   email: string = 'support@bacancy.com';
+  companyName: string = 'Bacancy © 2022';
+  footerImg: string = '../../assets/logo.png';
+  footerLinks: [string, string, string, string, string, string] = ['Home', 'Blog', 'Pricing', 'About', 'Faq', 'Contact']
 }
